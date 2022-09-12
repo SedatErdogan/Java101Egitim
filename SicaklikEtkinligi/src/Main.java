a6338ec752430331b06c40ff895c5f4fd6033394
SedatErdogan <sedat.erdogan@netas.com.tr>
2022-09-12 22:33:05 +0300

    Koşullu İfadeler ve Kod Blokları

---
+++ SicaklikEtkinligi/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sicaklik;
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Lütfen sıcaklık değeri giriniz:");
+        sicaklik=input.nextInt();
+
+        if (sicaklik<5) {
+            System.out.println("Kayak yapabilirsiniz.");
+
+
+        }else if(sicaklik<=5 && sicaklik<10){
+            System.out.println("Sinemaya gidebilirsiniz.");
+
+        } else if(sicaklik>=10 && sicaklik<=15){
+            System.out.println("Sinemaya gidebilirsiniz veya kayak yapabilirsiniz.");
+
+        }else if(sicaklik>15 && sicaklik<=25){
+            System.out.println("Piknik yapabilirsiniz.");
+        }else{
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+
+
+    }
+
+}
